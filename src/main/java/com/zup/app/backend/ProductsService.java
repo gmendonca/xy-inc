ff6f8bc48a1cdf25bfe20c06f9adb3e9e0f6106e
Gustavo Mendonca <gustavogmend@gmail.com>
2016-03-30 13:55:18 -0500

    Comments included and minor fixes for working http/get all request

--- src/main/java/com/zup/app/backend/ProductsService.java
+++ src/main/java/com/zup/app/backend/ProductsService.java
@@ -10,26 +10,31 @@ import com.zup.app.backend.model.Products;
 
 @Path("/products")
 public class ProductsService {
-	
+
 	@GET
-	  @Path("/all")
-	  @Produces(MediaType.TEXT_PLAIN)
-	  public String getAllProducts() {
-		Connector.openConnection();
-	    String result = "---Products List---\n";
-	    for(Products product : Connector.getAllProducts()){
-	    	result += product.getName();
-	    	result += "\n";
-	    	result += product.getDescription();
-	    	result += "\n";
-	    	result += product.getPrice();
-	    	result += "\n";
-	    	result += product.getCategory();
-	    	result += "\n";
-	    }
-	    Connector.closeConnection();
-				
-	    return result;    
-	  }
+	@Path("/all")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getAllProducts() {
+		String result = "---Products List---\n";
+		try {
+			Connector.openConnection();
+			for(Products product : Connector.getAllProducts()){
+				result += product.getName();
+				result += "\n";
+				result += product.getDescription();
+				result += "\n";
+				result += product.getPrice();
+				result += "\n";
+				result += product.getCategory();
+				result += "\n";
+			}
+		}catch (Exception e){
+			result += e.getMessage();
+		}finally{
+			Connector.closeConnection();
+		}
+
+		return result;
+	}
 
 }
