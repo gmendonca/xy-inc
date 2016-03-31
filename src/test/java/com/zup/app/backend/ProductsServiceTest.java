0738baf41ef87a187cc34736e1582fcdf1e90fea
Gustavo Mendonca <gustavogmend@gmail.com>
2016-03-31 10:52:28 -0500

    JUnit testes for get all done and working, starting other tests

--- src/test/java/com/zup/app/backend/ProductsServiceTest.java
+++ src/test/java/com/zup/app/backend/ProductsServiceTest.java
@@ -2,112 +2,57 @@ package com.zup.app.backend;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Map;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Application;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.zup.app.backend.model.Connector;
-import com.zup.app.backend.model.Product;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 
-public class ProductsServiceTest extends JerseyTest{
-	
-	//simple hello word test
-    @Path("hello")
-    public static class HelloResource {
-        @GET
-        public String getHello() {
-            return "Hello World!";
-        }
-    }
+public class ProductsServiceTest{
 
-    @Test
-    public void test() {
-        final String hello = target("hello").request().get(String.class);
-        assertEquals("Hello World!", hello);
-    }
-    
-    //test for add a product
-	/*@Path("allproducts")
-	public static class AddProductResource {
-	
-	    @POST
-	    @Produces(MediaType.APPLICATION_JSON)
-	    @Consumes(MediaType.APPLICATION_JSON)
-	    public String doPost(Map<String, String> data) {
-	        return "Hello " + data.get("name") + "!";
-	    }
-	
+	private HttpServer server;
+	private WebTarget target;
+
+	@Before
+	public void setUp() throws Exception {
+		// start the server
+		server = Main.startServer();
+		// create the client
+		Client c = ClientBuilder.newClient();
+
+		// uncomment the following line if you want to enable
+		// support for JSON in the client (you also have to uncomment
+		// dependency on jersey-media-json module in pom.xml and Main.startServer())
+		// --
+		c.register(JacksonJaxbJsonProvider.class);
+
+		target = c.target(Main.BASE_URI);
 	}
-	
-	@Test
-	public void testPost() {
-	    Product product = new Product();
-	    product.setName("iPhone 6");
-	    product.setDescription("This is an apple phone");
-	    product.setPrice(600.0f);
-	    product.setCategory("Smartphones");
-	
-	    final String hello = target("hello")
-	            .request()
-	            .post(Entity.json(product.toDocument()), String.class);
-	
-	    assertEquals("Hello popovitsj!", hello);
-	}*/
-	
-	//test for get all products
-	@Path("allproducts")
-	public static class AllProductsResource {
-		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		public ArrayList<Product> getAllProducts() {
-			ArrayList<Product> products = new ArrayList<Product>();
-			try {
-				Connector.openConnection();
-				products = Connector.getAllProducts();
-			}catch (Exception e){
-				return null;
-			}finally{
-				Connector.closeConnection();
-			}
 
-			return products;
-		}
-		
+	@After
+	public void tearDown() throws Exception {
+		server.shutdownNow();;
 	}
-	
+
 	@Test
 	public void testGetAll() {
-		
-		final Response response = target("allproducts")
-				.request(MediaType.APPLICATION_JSON_TYPE)
+
+		final Response response = target.path("/products/all")
+				.request(MediaType.APPLICATION_JSON)
 				.get();
-		
-		System.out.println(response.toString());
-		
-		assertEquals("Hello popovitsj!", response);
-	}
-	
-	@Override
-	protected Application configure() {
-	    return new ResourceConfig(HelloResource.class);
+
+		assertEquals(200, response.getStatus());
+		assertEquals("application/json", response.getMediaType());
+		//System.out.println("================");
+		//System.out.println(response.readEntity(String.class));
 	}
 
-
-    
-    
 }
