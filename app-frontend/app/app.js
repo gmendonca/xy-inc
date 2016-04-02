6112d4ec4e3bbe865810e7c39868905f0c2c2c9a
Gustavo Mendonca <gustavogmend@gmail.com>
2016-04-01 22:13:30 -0500

    organing, add .gitignore for mac and advancing towards frontend

--- app-frontend/app/app.js
+++ app-frontend/app/app.js
@@ -1,5 +1,25 @@
 var app = angular.module('myApp', ['ngRoute']);
 
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/getproduct', {
+        templateUrl: 'partials/getproduct.html',
+        controller: 'HttpCtrl'
+      }).
+      when('/getproducts', {
+        templateUrl: 'partials/getproducts.html',
+        controller: 'HttpCtrl'
+      }).
+      when('/addproduct', {
+        templateUrl: 'partials/addproducts.html',
+        controller: 'HttpCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
 app.controller("HttpCtrl", function($scope, $http){
 
 $scope.getProduct = function(id) {
@@ -8,6 +28,22 @@ $scope.getProduct = function(id) {
 
       response.success(function(data) {
           $scope.prod = data;
+        })
+
+      response.error(function(data, status, headers, config) {
+        alert("Failed to get data, status=" + status);
+      }
+  )};
+});
+
+app.controller("ProductsCtrl", function($scope, $http){
+
+$scope.getProduct = function(id) {
+
+      var response = $http.get('http://localhost:8080/myapp/products/');
+
+      response.success(function(data) {
+          $scope.products = data;
         })
 
       response.error(function(data, status, headers, config) {
